e007be072ed5d4f0122bbb2a5356cf36824e96cd
Lany <lany192@qq.com>
2016-12-18 23:13:32 +0800

    Initial commit

---
+++ src/com/lany/plugin/ClassModel.java
@@ -0,0 +1,25 @@
+package com.lany.plugin;
+
+/**
+ * Created by Lany on 2016/12/18.
+ */
+public class ClassModel {
+    private String _className;
+    private String _classFullName;
+
+    public String get_classFullName() {
+        return _classFullName;
+    }
+
+    public void set_classFullName(String _classFullName) {
+        this._classFullName = _classFullName;
+    }
+
+    public String get_className() {
+        return _className;
+    }
+
+    public void set_className(String _className) {
+        this._className = _className;
+    }
+}
